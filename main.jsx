4497fc144c14678fb8645a215bcf1b631ea076bf
SeokHyeonLee9 <ljk0197@gmail.com>
2025-04-12 01:41:30 +0900

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -1,1 +1,10 @@
-import React from 'react'; import ReactDOM from 'react-dom/client'; import EasyRunLive from './EasyRunLive'; import './index.css'; ReactDOM.createRoot(document.getElementById('root')).render(<EasyRunLive />);
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import EasyRunLive from './EasyRunLive';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <EasyRunLive />
+  </React.StrictMode>
+);
